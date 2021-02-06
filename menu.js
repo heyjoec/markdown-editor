f462da781f611c2c268368936fb7f123cdaf6b9d
JC <joe.chestnutt@gmail.com>
2021-02-06 22:16:17 +0000

    Added code to save on Ctrl-S

--- menu.js
+++ menu.js
@@ -8,19 +8,62 @@ const {
   dialog
 } = require('electron');
 
+const fs = require('fs');
 
+function saveFile() {
+  console.log('Saving the file');
+
+  const window = BrowserWindow.getFocusedWindow();
+  window.webContents.send('editor-event', 'save');
+}
+
+function loadFile() {
+  const window = BrowserWindow.getFocusedWindow();
+  const files = dialog.showOpenDialogSync(window, {
+      properties: ['openFile'],
+      title: 'Pick a markdown file',
+      filters: [{ name: 'Markdown', extensions: ['md', 'markdown','txt'] }]
+
+  });
+  if (!files) return;
+
+  const file = files[0];
+  const fileContent = fs.readFileSync(file).toString();
+  console.log(fileContent);
+  window.webContents.send('load', fileContent);
+}
 
 app.on('ready', () => {
   globalShortcut.register('CommandOrControl+S', () => {
     console.log('Saving the file');
-ipcMain.on('save', (event, arg) => {
-  console.log(`Saving content of the file`);
-  console.log(arg);
+    saveFile();
+  });
+
+  globalShortcut.register('CommandOrControl+O', () => {
+    loadFile();
+  });
 });
 
-const window = BrowserWindow.getFocusedWindow();
-  window.webContents.send('editor-event', 'save');
-  });
+ipcMain.on('save', (event, arg) => {
+    console.log(`Saving content of the file`);
+    console.log(arg); 
+
+    const window = BrowserWindow.getFocusedWindow();
+    const options = {
+        title: 'Save markdown file',
+        filters: [
+        {
+            name: 'MyFile',
+            extensions: ['md']
+        }
+        ]
+    };
+
+  const filename = dialog.showSaveDialogSync(window, options);
+  if (filename) {
+    console.log(`Saving content to the file: ${filename}`);
+    fs.writeFileSync(filename, arg);
+  }
 });
 
 ipcMain.on('editor-reply', (event, arg) => {
@@ -73,34 +116,34 @@ const template = [
     }
 ];
 
-    if (process.env.DEBUG) {
-        template.push({
-          label: 'Debugging',
-          submenu: [
-            {
-              label: 'Dev Tools',
-              role: 'toggleDevTools'
-            },
-      
-            { type: 'separator' },
-            {
-              role: 'reload',
-              accelerator: 'Alt+R'
-            }
-          ]
-        });
-      }
+if (process.env.DEBUG) {
+  template.push({
+    label: 'Debugging',
+    submenu: [
+      {
+        label: 'Dev Tools',
+        role: 'toggleDevTools'
+      },
 
-        if (process.platform === 'win32') {
-        template.unshift({
-          label: app.getName(),
-          submenu: [
-            { role: 'about' },
-            { type: 'separator' },
-            { role: 'quit' }
-          ]
-        })
+      { type: 'separator' },
+      {
+        role: 'reload',
+        accelerator: 'Alt+R'
       }
+    ]
+  });
+}
+
+  if (process.platform === 'win32') {
+  template.unshift({
+    label: app.getName(),
+    submenu: [
+      { role: 'about' },
+      { type: 'separator' },
+      { role: 'quit' }
+    ]
+  })
+}
 
       const menu = Menu.buildFromTemplate(template);
 
