d28140cf349a6675b33e7a90d6d1e717020c1ce9
JC <joe.chestnutt@gmail.com>
2021-02-06 00:58:10 +0000

    Added save messages

--- menu.js
+++ menu.js
@@ -1,11 +1,31 @@
-const { app, Menu, shell } = require('electron');
+const {
+  app,
+  Menu,
+  shell,
+  ipcMain,
+  BrowserWindow,
+  globalShortcut,
+  dialog
+} = require('electron');
 
-const { ipcMain } = require('electron');
-const { BrowserWindow } = require('electron');
+
+
+app.on('ready', () => {
+  globalShortcut.register('CommandOrControl+S', () => {
+    console.log('Saving the file');
+ipcMain.on('save', (event, arg) => {
+  console.log(`Saving content of the file`);
+  console.log(arg);
+});
+
+const window = BrowserWindow.getFocusedWindow();
+  window.webContents.send('editor-event', 'save');
+  });
+});
 
 ipcMain.on('editor-reply', (event, arg) => {
-    console.log(`Received reply from web page: ${arg}`);
-  });
+  console.log(`Received reply from web page: ${arg}`);
+});
 
 const template = [
     {
@@ -26,10 +46,7 @@ const template = [
                 label: 'Toggle Bold',
                 click() {
                     const window = BrowserWindow.getFocusedWindow();
-                    window.webContents.send(
-                        'editor-event', 
-                        'toggle-bold'
-                    )
+                    window.webContents.send('editor-event', 'toggle-bold');
                 }
             },
             {
@@ -56,19 +73,6 @@ const template = [
     }
 ];
 
-  {
-        if (process.platform === 'win32') {
-        template.unshift({
-          label: app.getName(),
-          submenu: [
-            { role: 'about' },
-            { type: 'separator' },
-            { role: 'quit' }
-          ]
-        })
-      }
-    }
-
     if (process.env.DEBUG) {
         template.push({
           label: 'Debugging',
@@ -85,6 +89,17 @@ const template = [
             }
           ]
         });
+      }
+
+        if (process.platform === 'win32') {
+        template.unshift({
+          label: app.getName(),
+          submenu: [
+            { role: 'about' },
+            { type: 'separator' },
+            { role: 'quit' }
+          ]
+        })
       }
 
       const menu = Menu.buildFromTemplate(template);
